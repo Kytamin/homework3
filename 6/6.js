e5ef31162354dbd17a4652eb964310b0a5f03edc
Kytamin <125472671+Kytamin@users.noreply.github.com>
2023-03-26 16:15:23 +0700

    Update 6.js

--- 6/6.js
+++ 6/6.js
@@ -1,8 +1,8 @@
 function checkValue(arr) {
     let k = +prompt("")
-    arr.sort((a, b) => b - a)
+    arr.sort((a, b) => b - a)    // sắp xếp lại mảng arr theo thứ tự giảm dần
 
-    return arr[k - 1]
+    return arr[k - 1]     //  giá trị lớn nhất khi này sẽ ở vị trí (1-1) tại vị trí 0 trong mảng, lớn thứ 2 sẽ là vị trí (2-1) trong mảng
 }
 
 let arr = [15, 30, 60, 20, 40, 90, 200, 180]
