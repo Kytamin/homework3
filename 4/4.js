c731d48c707e246c088d2e3f9e3b4362b7b228c9
Kytamin <125472671+Kytamin@users.noreply.github.com>
2023-03-26 16:38:49 +0700

    Update 4.js

--- 4/4.js
+++ 4/4.js
@@ -1,5 +1,5 @@
-function searchValue () {
-   let arr = [1,1,4,4,2,2,2,3,1,1]
+function searchValue (arr) {
+   
    let foundedValue
    for (let i = 0; i < arr.length; i++) {
       if (i === arr.indexOf(arr[i]) && i === arr.lastIndexOf(arr[i])) {
@@ -11,4 +11,5 @@ function searchValue () {
    }
    return foundedValue
 }
-console.log(`Search value: ${searchValue()}`)
+let arr1 = [1,1,4,4,2,2,2,3,1,1]
+console.log(`Search value: ${searchValue(arr1)}`)
